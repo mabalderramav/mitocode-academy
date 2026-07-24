46f799c80ec164dc60009d0fd40fc7b740a48ad2
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-07-24 12:40:46 -0400

    base project

---
+++ src/main/java/com/mitocode/academy/MitocodeAcademyApplication.java
@@ -0,0 +1,12 @@
+package com.mitocode.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitocodeAcademyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MitocodeAcademyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mitocode/academy/MitocodeAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitocodeAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
